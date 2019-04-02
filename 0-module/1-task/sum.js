eb24d9520c47783c4fa6cda193449a1875cda7df
lena <sample@qwe.com>
2019-04-02 13:16:36 +0300

    done 1-task for 0-module

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,14 @@
 function sum(a, b) {
-  /* ваш код */
+  if (isNumber(a) && isNumber(b)) {
+    return a + b;
+  }
+  throw new TypeError('arguments are not numbers');  
+}
+
+function isNumber(n) {
+  if (typeof n === 'number') return true;
+  if (typeof n === 'object' && typeof n.valueOf() === 'number') return true;
+  return false;
 }
 
 module.exports = sum;
